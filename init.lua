cb9e5724a16167714f45d11d44270d9455c967d7
Reto Baumgartner <reto.baumgartner@zi.uzh.ch>
2025-03-13 09:58:00 +0100

    add nvim-surround

--- init.lua
+++ init.lua
@@ -284,6 +284,17 @@ require('lazy').setup({
     },
   },
 
+  {
+    'kylechui/nvim-surround',
+    version = '*', -- Use for stability; omit to use `main` branch for the latest features
+    event = 'VeryLazy',
+    config = function()
+      require('nvim-surround').setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
+
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
