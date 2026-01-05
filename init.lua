b0958d9e77fd28060052a795791dffc690f8540a
rfbaumgartner <rfbaumgartner@users.noreply.github.com>
2026-01-05 10:45:24 +0100

    add gen.nvim

--- init.lua
+++ init.lua
@@ -299,6 +299,10 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'David-Kunz/gen.nvim',
+  },
+
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
