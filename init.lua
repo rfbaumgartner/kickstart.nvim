0aad3cf9219c2c0195c5f1aa420909781797563a
rfbaumgartner <rfbaumgartner@users.noreply.github.com>
2026-01-05 11:56:48 +0100

    fix name of ts server

--- init.lua
+++ init.lua
@@ -722,7 +722,7 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
-        tsserver = {},
+        ts_ls = {},
 
         lua_ls = {
           -- cmd = { ... },
